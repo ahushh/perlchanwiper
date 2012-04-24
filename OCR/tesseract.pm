a9f90bfe9fb30d2b70dc774d6cd50b3b257201c6
ahushh <ahushh@gmail.com>
2012-04-24 07:49:35 +0400

    fixed tesseract crashing, small fixes

--- OCR/tesseract.pm
+++ OCR/tesseract.pm
@@ -6,14 +6,19 @@ use Image::OCR::TesseractX 'get_ocr';
 sub decode_captcha($$)
 {
     my ($captcha_decode, $file_path) = @_;
+
     my $tmp  = tempdir('tesseractXXXX',  TMPDIR => 1, CLEANUP => 1);
-    my $text = get_ocr($file_path, $tmp, $captcha_decode->{lang}, $captcha_decode->{config});
+    my $text;
+    eval {
+        $text = get_ocr($file_path, $tmp, $captcha_decode->{lang}, $captcha_decode->{config});
+    };
+    warn $@ if $@;
 
     $text =~ s/^\s*//;
     $text =~ s/\s*$//;
     $text =~ s/\n//;
 
-    return $text;
+    return $text || undef;
 }
 
 sub abuse($$) { }
